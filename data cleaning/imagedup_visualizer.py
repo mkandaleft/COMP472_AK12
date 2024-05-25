6cac53da34309590bbd9023fd38f06df9434d5a4
tinatsl <91174232+tinatsl@users.noreply.github.com>
2024-05-25 19:32:00 -0400

    Adjusted similarity threshold for duplicate identification

--- data cleaning/imagedup_visualizer.py
+++ data cleaning/imagedup_visualizer.py
@@ -17,17 +17,28 @@ encodings = cnn.encode_images(image_dir=img_path)
 # Identify img that have duplicates
 duplicates = cnn.find_duplicates(encoding_map=encodings, scores=True)
 
+threshold = 0.97
+
+# Filter duplicates based on the threshold
+filtered_duplicates = {}
 for key, value in duplicates.items():
+    filtered_value = [dup for dup in value if dup[1] >= threshold]
+    if filtered_value:
+        filtered_duplicates[key] = filtered_value
+
+print(len(filtered_duplicates))
+
+# Iterate through the duplicates dictionary
+for key, value in filtered_duplicates.items():
     if len(value) > 0:
-        print(key + ",")
         # Delete duplicate values
-        # for duplicate_tuple in value:
-        #     duplicate = duplicate_tuple[0]  # Extract filename, need string
-        #     duplicate_path = os.path.join(img_path, duplicate)
-        #     if os.path.exists(duplicate_path):
-        #         os.remove(duplicate_path)
+        for duplicate_tuple in value:
+            duplicate = duplicate_tuple[0]  # Extract filename, need string
+            duplicate_path = os.path.join(img_path, duplicate)
+            if os.path.exists(duplicate_path):
+                os.remove(duplicate_path)
 
 # To visualize an arbitrary image and its duplicates
-plot_duplicates(image_dir=img_path,
-                duplicate_map=duplicates,
-                filename='1001.png')
+# plot_duplicates(image_dir=img_path,
+#                 duplicate_map=duplicates,
+#                 filename='1001.png')
