b877eeb5ddfc37e3314cd81b86e96bc3ba1a235c
tinatsl <v_tsilin@live.concordia.ca>
2024-06-17 23:27:21 -0400

    Added confidence score to prediction.py

--- model_training/prediction.py
+++ model_training/prediction.py
@@ -1,7 +1,10 @@
 from PIL import Image
+from sklearn.metrics import accuracy_score
+
 from mainModel import SimpleCNN
 import torch
 import torchvision.transforms as transforms
+import torch.nn.functional as Funct
 
 transform = transforms.Compose([
     transforms.Resize((48, 48)),
@@ -17,16 +20,35 @@ def predict_image(model, image_tensor, device, class_labels):
     model.eval()  # Set the model to evaluation mode
     with torch.no_grad():
         output = model(image_tensor)
-        _, predicted = torch.max(output, 1)
-    return class_labels[predicted.item()]
+        probabilities = Funct.softmax(output, dim=1)
+        confidence, predicted = torch.max(probabilities, 1)
+    return class_labels[predicted.item()], confidence.item()
 
 
 # Function to load the image using transformation from dataLoader
 def load_image(image_path):
-    image = Image.open(image_path).convert('RGB')  # Ensure correct image mode
-    image = transform(image).unsqueeze(0)  # Add batch dimension
+    image = Image.open(image_path).convert('RGB')
+    image = transform(image).unsqueeze(0)
     return image
 
+
+# Function to calculate accuracy
+# def calculate_accuracy(model, data_loader, device):
+#     model.to(device)
+#     model.eval()
+#     all_preds = []
+#     all_labels = []
+#
+#     with torch.no_grad():
+#         for images, labels in data_loader:
+#             images, labels = images.to(device), labels.to(device)
+#             outputs = model(images)
+#             _, predicted = torch.max(outputs, 1)
+#             all_preds.extend(predicted.cpu().numpy())
+#             all_labels.extend(labels.cpu().numpy())
+#
+#     accuracy = accuracy_score(all_labels, all_preds)
+#     return accuracy
 
 if __name__ == "__main__":
     model_path = 'best_model.pth'
@@ -45,5 +67,5 @@ if __name__ == "__main__":
     class_labels = ["angry", "focused", "happy", "neutral"]
 
     # Make prediction on img
-    prediction = predict_image(model, image_tensor, device, class_labels)
-    print(f'Predicted class: {prediction}')
+    predicted_class, confidence_score = predict_image(model, image_tensor, device, class_labels)
+    print(f'Predicted class: {predicted_class}, Confidence Score = {confidence_score:.4f}')
