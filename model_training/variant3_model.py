9d9858094a3a32a2fb3c03201e3ffa0a54e32e31
mkandaleft <markandaleft@hotmail.com>
2024-06-27 20:33:59 +0000

    Update variant3_model.py

--- model_training/variant3_model.py
+++ model_training/variant3_model.py
@@ -6,51 +6,37 @@ import torch.nn.functional as F
 class SimpleCNN(nn.Module):
     def __init__(self, num_classes=6):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.dropout1 = nn.Dropout(0.3)
         self.bn2 = nn.BatchNorm2d(128)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2)
         self.bn3 = nn.BatchNorm2d(256)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-
         self.conv4 = nn.Conv2d(256, 256, kernel_size=5, stride=1, padding=2)
         self.bn4 = nn.BatchNorm2d(256)
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.flatten = nn.Flatten()
 
         self.fc1 = nn.Linear(256 * 3 * 3, 128)  # Adjusted for k
         self.bn5 = nn.BatchNorm1d(128)
         self.dropout2 = nn.Dropout(0.25)
-
         self.fc2 = nn.Linear(128, 64)
         self.bn6 = nn.BatchNorm1d(64)
-
         self.fc3 = nn.Linear(64, num_classes)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
-        x = self.pool1(x)
-
         x = self.dropout1(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool1(x)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
         x = self.pool2(x)
 
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.pool3(x)
-
-        x = F.relu(self.bn4(self.conv4(x)))
-        x = self.pool4(x)
-
         x = self.flatten(x)
-
         x = self.dropout2(F.relu(self.bn5(self.fc1(x))))
         x = F.relu(self.bn6(self.fc2(x)))
-
         x = self.fc3(x)
         return x
